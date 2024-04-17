6a7916d2156fd3557beedf869d13fde21ed12371
gayomii <gayeon4041@gmail.com>
2024-04-17 14:47:39 +0900

    init react-native project

---
+++ SNSProject/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, SafeAreaView } from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView style={{ backgroundColor: '#fff' }}>
+      <Text>React Native</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
