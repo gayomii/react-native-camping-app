f9de3eb1b5ce2acb53627e05741107cdb96ed4de
gayomii <gayeon4041@gmail.com>
2024-04-18 01:25:15 +0900

    style: add assets

---
+++ SNSProject/src/constants/images.ts
@@ -0,0 +1,8 @@
+import { ImageRequireSource } from 'react-native';
+
+export const bottomImages: { [key: string]: ImageRequireSource } = {
+  camping: require('../assets/bottomIcons/campingIcon.png'),
+  community: require('../assets/bottomIcons/communityIcon.png'),
+  article: require('../assets/bottomIcons/articleIcon.png'),
+  settings: require('../assets/bottomIcons/settingsIcon.png'),
+};
