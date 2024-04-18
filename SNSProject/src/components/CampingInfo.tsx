6943dd31043581b8d9fbf522b92955e666716100
gayomii <gayeon4041@gmail.com>
2024-04-18 19:18:43 +0900

    feat: get camping data & list ui

---
+++ SNSProject/src/components/CampingInfo.tsx
@@ -0,0 +1,128 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  Dimensions,
+} from 'react-native';
+
+type CampingInfoProps = {
+  facltNm?: string;
+  addr1?: string;
+  facltDivNm?: string;
+  mangeDivNm?: string;
+  resveCl?: string;
+  caravInnerFclty?: string;
+  intro?: string;
+  induty?: string;
+  firstImageUrl?: string;
+};
+
+const { width, height } = Dimensions.get('window');
+
+const CampingInfo = ({
+  facltNm,
+  addr1,
+  facltDivNm,
+  mangeDivNm,
+  resveCl,
+  caravInnerFclty,
+  intro,
+  induty,
+  firstImageUrl,
+}: CampingInfoProps) => {
+  return (
+    <TouchableOpacity style={styles.wrapper}>
+      <Image source={{ uri: firstImageUrl }} style={styles.campingImg} />
+      <View style={styles.indutyContainer}>
+        {induty.split(',').map(item => (
+          <View style={styles.indutyTextBox}>
+            <Text style={styles.indutyText}>{item}</Text>
+          </View>
+        ))}
+      </View>
+      <View style={styles.divNmContainer}>
+        <Text style={styles.divNmText}>{facltDivNm}</Text>
+        <Text style={styles.divNmText}>{mangeDivNm}</Text>
+      </View>
+      <View style={styles.nameContainer}>
+        <Text style={styles.nameText}>{facltNm}</Text>
+      </View>
+      <View style={styles.subInfoContainer}>
+        <Text style={styles.addrText}>{addr1}</Text>
+        <Text style={styles.resveText}>{resveCl}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  wrapper: {
+    paddingHorizontal: 8,
+    paddingVertical: 12,
+    marginVertical: 12,
+    borderRadius: 4,
+    backgroundColor: '#fff',
+  },
+  campingImg: {
+    height: width / 2,
+    borderRadius: 4,
+    marginBottom: 8,
+  },
+  indutyContainer: {
+    position: 'absolute',
+    zIndex: 10,
+    top: 20,
+    left: 20,
+    flexDirection: 'row',
+    gap: 10,
+  },
+  indutyTextBox: {
+    paddingHorizontal: 12,
+    paddingVertical: 4,
+    borderRadius: 4,
+    backgroundColor: '#FC9D45',
+  },
+  indutyText: {
+    color: '#fff',
+    fontWeight: '600',
+    fontSize: 14,
+  },
+  divNmContainer: {
+    flexDirection: 'row',
+    gap: 8,
+    marginBottom: 6,
+  },
+  divNmText: {
+    fontSize: 13,
+    fontWeight: '600',
+    color: '#919191',
+  },
+  nameContainer: {
+    marginBottom: 35,
+  },
+  nameText: {
+    fontSize: 18,
+    fontWeight: '600',
+    color: '#545454',
+  },
+  subInfoContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  addrText: {
+    fontSize: 13,
+    fontWeight: '600',
+    color: '#707070',
+  },
+  resveText: {
+    fontSize: 18,
+    fontWeight: '600',
+    color: '#FC9D45',
+  },
+});
+
+export default CampingInfo;
--- SNSProject/src/pages/home/HomePage.tsx
+++ SNSProject/src/pages/home/HomePage.tsx
@@ -1,12 +1,42 @@
-import React from 'react';
-import { Text, SafeAreaView, StyleSheet } from 'react-native';
-import Icon from 'react-native-vector-icons/Entypo';
+import React, { useEffect, useState } from 'react';
+import { View, Text, SafeAreaView, StyleSheet, Image, FlatList } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import { getCampingList } from '../../apis/campingApi';
+import CampingInfo from '../../components/CampingInfo';
+import MyHeader from '../../components/MyHeader';
 
 const HomePage = () => {
+  const [campingList, setCampingList] = useState([]);
+
+  useEffect(() => {
+    const fetchCampingList = async () => {
+      const result = await getCampingList();
+      setCampingList(result.response.body.items.item);
+    };
+
+    fetchCampingList();
+  }, []);
+
   return (
     <SafeAreaView style={styles.wrapper}>
-      <Icon name="arrow-bold-right" color="#000" size={24} />
-      <Text>home page</Text>
+      <View>
+        <MyHeader
+          title="캠핑투게더"
+          leftIcon={<Icon name="grip-lines" size={18} />}
+          // TODO: Image일때, style
+          rightIcon={<Icon name="search" size={18} />}
+        />
+      </View>
+      <View style={styles.mainContainer}>
+        <FlatList
+          data={campingList}
+          keyExtractor={item => item.contentId}
+          renderItem={({ item }) => <CampingInfo {...item} />}
+          removeClippedSubviews
+          showsVerticalScrollIndicator={false}
+          style={styles.campingListContainer}
+        />
+      </View>
     </SafeAreaView>
   );
 };
@@ -15,6 +45,14 @@ const styles = StyleSheet.create({
   wrapper: {
     flex: 1,
     backgroundColor: '#FFF3E9',
+  },
+  campingListContainer: {
+    paddingHorizontal: 16,
+  },
+  mainContainer: {
+    paddingBottom: 60,
+    backgroundColor: '#fff',
+    flex: 1,
   },
 });
 
